a31d1d7223d2b30f55b1633d316e8a302b674d1b
Ben Degeneve <poompoomcha@gmail.com>
2022-03-23 10:31:54 +0100

    App logic

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ Vue.createApp({
     return {
       submited: false,
       ratings: ["1", "2", "3", "4", "5"],
+      selected: undefined,
     };
   },
+  methods: {
+
+      handleSubmit() {
+        console.log(this.selected)
+          this.submited = true
+      }
+  }
 }).mount('#app');
